89900315f49b0e918f977e45d4fbee82be48f026
WimT <w.keizer@allsolutions.nl>
2020-05-01 10:59:59 +0200

    Add overlay component to show the suggestions

--- packages/atmention-angular/src/atmention.module.ts
+++ packages/atmention-angular/src/atmention.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AtmentionViewerComponent } from './components/atmention-viewer/atmention-viewer.component';
 import { AtmentionEditorComponent } from './components/atmention-editor/atmention-editor.component';
-import {AtmentionParse} from './pipes/atmention.pipe';
+import { AtementionSuggestionsOverlayComponent } from './components/atemention-suggestions-overlay/atemention-suggestions-overlay.component';
+import { AtmentionParse } from './pipes/atmention.pipe';
 
 @NgModule({
   declarations: [
     AtmentionViewerComponent,
     AtmentionEditorComponent,
+    AtementionSuggestionsOverlayComponent,
     AtmentionParse,
   ],
   imports: [
@@ -16,6 +18,7 @@ import {AtmentionParse} from './pipes/atmention.pipe';
   exports: [
     AtmentionViewerComponent,
     AtmentionEditorComponent,
+    AtementionSuggestionsOverlayComponent,
     AtmentionParse,
   ]
 })

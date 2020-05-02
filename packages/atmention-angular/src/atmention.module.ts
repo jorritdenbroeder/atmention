f609c7c85d1fc785351a1caa228f86cc8749a3ed
Jorrit den Broeder <jorrit.denbroeder@pixelpool.com>
2020-05-02 21:51:03 +0200

    Revert "Add overlay component to show the suggestions"
    This reverts commit 89900315f49b0e918f977e45d4fbee82be48f026.

--- packages/atmention-angular/src/atmention.module.ts
+++ packages/atmention-angular/src/atmention.module.ts
@@ -2,14 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AtmentionViewerComponent } from './components/atmention-viewer/atmention-viewer.component';
 import { AtmentionEditorComponent } from './components/atmention-editor/atmention-editor.component';
-import { AtementionSuggestionsOverlayComponent } from './components/atemention-suggestions-overlay/atemention-suggestions-overlay.component';
-import { AtmentionParse } from './pipes/atmention.pipe';
+import {AtmentionParse} from './pipes/atmention.pipe';
 
 @NgModule({
   declarations: [
     AtmentionViewerComponent,
     AtmentionEditorComponent,
-    AtementionSuggestionsOverlayComponent,
     AtmentionParse,
   ],
   imports: [
@@ -18,7 +16,6 @@ import { AtmentionParse } from './pipes/atmention.pipe';
   exports: [
     AtmentionViewerComponent,
     AtmentionEditorComponent,
-    AtementionSuggestionsOverlayComponent,
     AtmentionParse,
   ]
 })
